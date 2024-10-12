b2fe6922e2d1fabd23ce7d5274714998ddec2c82
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-10-12 14:31:20 -0700

    Build an API to handle the image uploading
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront3/store/serializers.py
+++ storefront3/store/serializers.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
 from .signals import order_created
-from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
+from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review, ProductImage
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -25,6 +25,14 @@ class ProductSerializer(serializers.ModelSerializer):
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    # overwrite the create method
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id, **validated_data)
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
